465833970b6538015d40ff512177e6b2ca45b905
Andre Henrique <andremeliunas@hotmail.com>
2022-07-27 11:27:23 -0300

    estudos 27-07

--- exibindo-livros.js
+++ exibindo-livros.js
@@ -1,31 +1,14 @@
-const precosLivros = [
-    {
-        titulo: "JavaScript",
-        preco: 25},
-    {
-        titulo: "PHP",
-        preco: 15},
-    {
-        titulo: "Java",
-        preco: 30},
-    {
-        titulo: "Elixir",
-        preco: 50},
-    {
-        titulo: "Go",
-        preco: 45},
-    {
-        titulo: "Python",
-        preco: 20}
-]
+const livros = require('/reaproveitando-codigos.js');
 
-let maisBarato = 0
-
-for ( let atual = 0; atual < precosLivros.length; atual++){
-    if (precosLivros[atual].preco < precosLivros[maisBarato].preco){
-        maisBarato = atual
+function menorValor(arrProdutos, posicaoInicial) {
+  let maisBarato = posicaoInicial;
+  
+  for (let atual = posicaoInicial; atual < arrProdutos.length; atual++) {
+    if (arrProdutos[atual].preco < arrProdutos[maisBarato].preco) {
+      maisBarato = atual
     }
+  }
+  return maisBarato;
 }
 
-console.log(`O livro mais barato custa ${precosLivros[maisBarato].preco} e seu nome é ${precosLivros[maisBarato].titulo}`)
-
+module.exports = menorValor;
