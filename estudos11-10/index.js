1899b5a797df44a561cbe25f0e2fd737a11eb235
Andre Henrique <andremeliunas@hotmail.com>
2022-10-11 11:27:53 -0300

    estudos11-10

---
+++ estudos11-10/index.js
@@ -0,0 +1,7 @@
+//const chalk = require('chalk')
+
+import chalk from 'chalk'
+
+console.log(chalk.blue('ola mundo!'))
+console.log('São geralmente recuperados a partir de um objeto [FileList](https://developer.mozilla.org/pt-BR/docs/Web/API/FileList) que é retornado como resultado da seleção, pelo usuário, de arquivos através do elemento [<input>](https://developer.mozilla.org/pt-BR/docs/Web/HTML/Element/Input)')
+console.log('São geralmente recuperados a partir de um objeto [FileList](https://developer.mozilla.org/pt-BR/docs/Web/API/FileList) que é retornado como resultado da seleção, pelo usuário, de arquivos através do elemento [<input>](https://developer.mozilla.org/pt-BR/docs/Web/HTML/Element/Input)')
